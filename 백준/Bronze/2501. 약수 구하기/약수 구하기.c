56b0fa8efde059b2e4db9dcfd5103a78798b0de5
와니 <chaewan120@naver.com>
2024-07-27 21:35:43 +0900

    7/27 (#33)
    * [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub
    
    * [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 1112 KB -BaekjoonHub
    
    * [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 1116 KB -BaekjoonHub
    
    * [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub
    
    * [Bronze I] Title: 단어 공부, Time: 16 ms, Memory: 1972 KB -BaekjoonHub
    
    * [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 1112 KB -BaekjoonHub
    
    * [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 40 ms, Memory: 31120 KB -BaekjoonHub
    
    * [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 31120 KB -BaekjoonHub
    
    * [Silver V] Title: 너의 평점은, Time: 48 ms, Memory: 31252 KB -BaekjoonHub
    
    * [Bronze III] Title: 행렬 덧셈, Time: 4 ms, Memory: 1116 KB -BaekjoonHub
    
    * [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 1112 KB -BaekjoonHub
    
    * [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub
    
    * [Silver V] Title: 색종이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub
    
    * [Bronze II] Title: 진법 변환, Time: 0 ms, Memory: 1300 KB -BaekjoonHub
    
    * [Bronze I] Title: 진법 변환 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub
    
    * [Bronze III] Title: 세탁소 사장 동혁, Time: 0 ms, Memory: 1116 KB -BaekjoonHub
    
    * [Bronze III] Title: 중앙 이동 알고리즘, Time: 0 ms, Memory: 1112 KB -BaekjoonHub
    
    * [Bronze II] Title: 벌집, Time: 0 ms, Memory: 1112 KB -BaekjoonHub
    
    * 2
    
    * [Silver V] Title: 분수찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub
    
    * [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub
    
    * [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+    int n, k, i, cnt = 0;
+    scanf("%d %d", &n, &k);
+    for (i = 1; i <= n; i++) {
+        if (n % i == 0)
+            cnt++;
+        if (cnt == k)
+            break;
+    }
+    if (i <= n) printf("%d", i);
+    else printf("0");
+}
