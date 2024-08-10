659d60442b63df22048fe06374514e30f9e0c856
와니 <chaewan120@naver.com>
2024-08-10 17:01:36 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -1,5 +1,5 @@
-a,b,v = map(int,input().split())
-if (v-b)%(a-b) == 0:
-    print((v-b)//(a-b))
+A,B,V = map(int,input().split())
+if (V-B) % (A-B) == 0:
+    print((V-B) // (A-B))
 else:
-    print((v-b)//(a-b)+1)
+    print((V-B) // (A-B) +1)
