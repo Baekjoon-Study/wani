eae8cc518aac6bfcd1b22290d026c62ce47592ce
와니 <chaewan120@naver.com>
2024-08-13 15:42:23 +0900

    [Bronze I] Title: 소인수분해, Time: 1096 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,8 +1,5 @@
 n = int(input())
-if n == 1:
-    print('')
-for i in range(2, n+1):
-    if n % i == 0:
-        while n % i == 0:
-            print(i)
-            n = n / i
+for i in range(2,n+1):
+    while n % i == 0:
+        n = n//i
+        print(i)
