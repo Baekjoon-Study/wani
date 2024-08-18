c84cc4c4c0cf50cfa9d4b650b99e70f0eea93ee0
와니 <chaewan120@naver.com>
2024-08-18 20:57:49 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.c
+++ 백준/Bronze/2753. 윤년/윤년.c
@@ -1,11 +1,7 @@
-#include <stdio.h>
 int main() {
-	int a;
-	scanf("%d", &a);
-	if (a % 4 == 0 && a % 100 != 0 || a % 400 == 0) {
-		printf("1");
-	}
-	else {
-		printf("0");
-	}
+	int year;
+	scanf("%d", &year);
+	if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)printf("1\n");
+	else printf("0\n");
+	return 0;
 }
