c3582cb5bb17bbcea481bd3bfd6dbd6db64d3908
와니 <chaewan120@naver.com>
2024-08-18 20:51:20 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.c
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.c
@@ -2,13 +2,8 @@
 int main() {
 	int a, b;
 	scanf("%d %d", &a, &b);
-	if (a > b) {
-		printf(">");
-	}
-	else if (a < b) {
-		printf("<");
-	}
-	else {
-		printf("==");
-	}
+	if (a > b)printf(">\n");
+	else if (a < b)printf("<\n");
+	else printf("==\n");
+	return 0;
 }
