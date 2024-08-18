3dcc27fb09e7ac4fef5a21b45e2bdaf39a9ce38d
와니 <chaewan120@naver.com>
2024-08-18 21:18:35 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.c
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 int main() {
 	int x, y;
-	scanf("%d\n%d", &x, &y);
-	if (x > 0 && y > 0) {
-		printf("1");
-	}
-	else if (x < 0 && y > 0) {
-		printf("2");
-	}
-	else if (x < 0 && y < 0) {
-		printf("3");
-	}
-	else {
-		printf("4");
-	}
+	scanf("%d", &x);
+	scanf("%d", &y);
+	if (x > 0 && y > 0)printf("1\n");
+	else if (x < 0 && y>0)printf("2\n");
+	else if (x < 0 && y < 0)printf("3\n");
+	else printf("4\n");
+	return 0;
 }
