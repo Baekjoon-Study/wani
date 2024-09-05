da9b78d6779489dea1fab739d7f7779c82183f71
와니 <chaewan120@naver.com>
2024-09-05 21:37:47 +0900

    [Bronze II] Title: 커트라인, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/25305. 커트라인/커트라인.py
+++ 백준/Bronze/25305. 커트라인/커트라인.py
@@ -1,3 +1,4 @@
-N,k = map(int,input().split())
-scores = map(int,input().split())
-print(sorted(scores)[-k])
+n,k=map(int,input().split())
+num = [*map(int,input().split())]
+num.sort()
+print(num[-k])
