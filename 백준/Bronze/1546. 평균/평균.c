f86b4d946a05c04d834551c5182ddcd16688db89
와니 <chaewan120@naver.com>
2024-09-15 18:18:41 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.c
+++ 백준/Bronze/1546. 평균/평균.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-int main() {
-	int N, max = 0;
-    float avg = 0;
-    scanf("%d", &N);
-    int score[N];
-    for (int i = 0; i < N; i++) {
-        scanf("%d", &score[i]);
-        if (max < score[i]) {
-            max = score[i];
+int main(){
+    int n,i,max=0;
+    scanf("%d",&n);
+    int score[n];
+    float average;
+    for(i=0;i<n;i++){
+        scanf("%d",&score[i]);
+        if(max<score[i]){
+            max=score[i];
         }
     }
-    for (int i = 0; i < N; i++) {
-        avg += (float)score[i] / max * 100;
+    for(i=0;i<n;i++){
+        average += (float)score[i]/max*100;
     }
-    printf("%f\n", avg / N);
+    printf("%f",average/n);
 }
