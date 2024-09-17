53447b67d93da02888d61e5512f1ebdd6144602d
와니 <chaewan120@naver.com>
2024-09-18 00:54:36 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.c
+++ 백준/Bronze/2566. 최댓값/최댓값.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
-
+#define L 9
 int main(){
-    int num, max=0, row=0, column=0, a[9][9];
-    for (int i = 0; i < 9; i++){
-        for (int j = 0; j < 9; j++){
-            scanf("%d", &a[i][j]);
-        }
-    }
-    for (int i = 0; i < 9; i++){
-        for (int j = 0; j < 9; j++){
-            if (max < a[i][j]){
-                max = a[i][j];
-                row = i;
-                column = j;
+    int num_list[L][L],i,k;
+    int max=0, idx_column=0, idx_row=0;
+    for(i=0;i<L;i++){
+        for(k=0;k<L;k++){
+            scanf("%d",&num_list[i][k]);
+            if(num_list[i][k]>max){
+                max = num_list[i][k];
+                idx_column = i;
+                idx_row = k;
+           
             }
         }
     }
-    printf("%d \n", max);
-    printf("%d %d", row + 1, column + 1);
+    printf("%d\n",max);
+    printf("%d %d",idx_column+1,idx_row+1);
 }
